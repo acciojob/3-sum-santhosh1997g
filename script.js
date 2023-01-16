e4446f0dfd40adbf4001cb1a7b91b7b7bbe499eb
Guntur Santhosh Kumar <98687152+santhosh1997g@users.noreply.github.com>
2023-01-16 22:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function threeSum(arr, target) {
-  var sum = 0;
-for(var i=0;i<arr.length;i++){
-  sum= sum+arr[i];
-  if(sum == target){
-  return sum;
+function threeSumClosest(nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestSum = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === target) {
+                return target;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+            closestSum = Math.abs(sum - target) < Math.abs(closestSum - target) ? sum : closestSum;
+        }
+    }
+    return closestSum;
 }
-}
-}
-
-module.exports = threeSum;
