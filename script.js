2cf7fadb12ed5d79819d31cb7d32260f09c51f41
Guntur Santhosh Kumar <98687152+santhosh1997g@users.noreply.github.com>
2023-01-16 22:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function threeSum(arr, target) {
-  //your code here
+  var sum = 0;
+for(var i=0;i<arr.length;i++){
+  sum= sum+arr[i];
+  if(sum == target){
+  return sum;
+}
+}
 }
 
 module.exports = threeSum;
